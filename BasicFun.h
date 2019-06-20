82b3318aa31e9e7667b6eb17f5dddcc2663fcfa2
lyh9909 <51875551+lyh9909@users.noreply.github.com>
2019-06-20 22:09:04 +0800

    圆环识别进行更改

--- BasicFun.h
+++ BasicFun.h
@@ -22,6 +22,7 @@ Point SearchRightUpEage45(int row, int col);
 
 int GetLL(int row, int col);
 int GetRL(int row, int col);
+int CurveDetection(int up_row, int down_row, int type);
 int AveGray(void);
 
 int MaxArray(int* line, int n);
