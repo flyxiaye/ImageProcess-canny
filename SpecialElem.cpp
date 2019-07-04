9ad00b71f5c46952f5dd4b9bf01cda0284a4d53e
lyh9909 <51875551+lyh9909@users.noreply.github.com>
2019-07-05 00:30:24 +0800

    路障修改

--- SpecialElem.cpp
+++ SpecialElem.cpp
@@ -241,12 +241,14 @@ void ImgJudgeObstacle(void)
 			int Front = MIN(LeftPnt.ErrRow, RightPnt.ErrRow);
 			int FrontRompGray = RegionAveGray(Front - 10, LeftPnt.ErrCol + 5, RightPnt.ErrCol - 5);
 			int FrontBlockGray = RegionAveGray(Front - 10, LeftPnt.ErrCol, RightPnt.ErrCol);
+			int FrontBlockGray2 = RegionAveGray(Front - 2, LeftPnt.ErrCol, RightPnt.ErrCol);
 			int DownGray = RegionAveGray(DOWN_EAGE - 2, LL[DOWN_EAGE - 2], RL[DOWN_EAGE - 2]);
 			string.Format("\r\n FrontRompGray = %d \r\n", FrontRompGray); PrintDebug(string);
 			string.Format("\r\n FrontBlockGray = %d \r\n", FrontBlockGray); PrintDebug(string);
+			string.Format("\r\n FrontBlockGray2 = %d \r\n", FrontBlockGray2); PrintDebug(string);
 			string.Format("\r\n DownGray = %d \r\n", DownGray); PrintDebug(string);
-			if (DownGray - FrontBlockGray > DarkThreshold &&
-				FrontRompGray - FrontBlockGray < 6 && FrontBlockGray - FrontRompGray < 6)
+			if (DownGray - FrontBlockGray > DarkThreshold && DownGray - FrontBlockGray2 > DarkThreshold
+				&& FrontRompGray - FrontBlockGray < 6 && FrontBlockGray - FrontRompGray < 6)
 			{
 				Img_BlockFlag = 1;//Â·ÕÏ
 				Img_SpecialElemFlag = 1;
